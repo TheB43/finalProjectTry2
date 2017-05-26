19d2694919d1d4394de864b4b2a5f6d305fbf4d1
TheB43 <dheillyg3482@gmail.com>
2017-05-26 10:52:01 -0700

    Added the neccesary images and the scorecounter class

--- Casino/mainMenu.java
+++ Casino/mainMenu.java
@@ -1,25 +1,12 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class mainMenu here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class mainMenu extends World
 {
-
-    /**
-     * Constructor for objects of class mainMenu.
-     * 
-     */
     public mainMenu()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(900, 550, 1); 
-    }   
+    {
+        super(900, 550, 1);
+    }
     public void startMenu() {
-        getBackground().drawImage(new GreenfootImage("Main Menu.png"), 0, 0);
+        addObject(new scoreCounter(), 60, 20);
         Greenfoot.start();
     }
     public void act() {
--- Casino/scoreCounter.java
+++ Casino/scoreCounter.java
@@ -1,13 +1,10 @@
 import greenfoot.*;
 import java.awt.Color;  
-
 public class scoreCounter extends Actor
 {
-    int Startcash = 200;
-    public String userCash() {
-        String output = new String();
-        output = "$" + Startcash;
-        return output;
+    public int Startcash = 200;
+    private String userCash() {
+        return "$" + Startcash;
     }
     public scoreCounter(){
         setImage(new GreenfootImage( "Score: " + userCash(), 25, Color.YELLOW, Color.BLACK));
