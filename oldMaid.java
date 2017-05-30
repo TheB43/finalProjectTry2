af094d18c28f218cd2ff673b2055867166c57f5b
B-C-Gallagher <b.c.gallagher24@gmail.com>
2017-05-30 10:23:02 -0700

    Add files via upload

--- oldMaid.java
+++ oldMaid.java
@@ -1,6 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo) test
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.awt.Color;
 
 /**
@@ -27,10 +26,19 @@ public class oldMaid extends World {
             GreenfootImage balanceInfo = new GreenfootImage("Your Balance is: " + userBalance 
                         + "\nPlace your bet (Minimum of $50): ", 12, Color.WHITE, Color.BLACK);
             
-            int userAdd = click("bet");
-            int userBet = 50 + userAdd;
+            //Place bet
+            boolean bet = false;
+            int userBet = 50;
+            while(bet == false && userBet < 50 ) { //Allows user to constantly update bet until they hit "BET" setting their amount
+                int userAdd = click("bet");
+                if(userAdd == -100) { //-100 is just some arbitrary number that cannot be reachde by any other means
+                    bet = true;
+                }
+                userBet += userAdd;
+                GreenfootImage showBet = new GreenfootImage(userBet + "\nPlace", 12, Color.WHITE, Color.BLACK);
+            }
             int dealerBet = userBet;
-        
+            
             //Instantiate one full deck
             ArrayList<Card> deck = new ArrayList<Card>(52);
             Deck OGdeck = new Deck();
@@ -158,7 +166,9 @@ public class oldMaid extends World {
         }
     }
     
-    public int click(String situation) {
+    public int click(String situation) {//Input each situation, whther it be choosing a card, placing a bet, or playing again.
+        int bet = 0;
+        int screenBet = 50 + bet;
         int returnNum = 0;
         
         MouseInfo mouse = Greenfoot.getMouseInfo();
@@ -167,8 +177,8 @@ public class oldMaid extends World {
             int y = mouse.getY();
         
             if(Greenfoot.mouseClicked(null)) {
-                if(situation.equals("cards")) {
-                    if (y > 0 && y < 94) {
+                if(situation.equals("cards")) { //This code is for choosing cards
+                    if (y > 456 && y < 550) {
                         if(x > (600 / 23) - 74 && x < 600 / 23) {
                                 returnNum = 900 / 23;
                         }
@@ -181,12 +191,21 @@ public class oldMaid extends World {
                         returnNum = 2;
                     }
                 }
-                if(situation.equals("bet")) {
-                    if(mouse.getButton() == 1) {
-                        returnNum = 10;
-                    }
-                    if(mouse.getButton() == 3) {
-                        returnNum = -10;
+                if(situation.equals("bet")) { //This code is for placing a bet.
+                    if(y > 233 && y < 550 || y < 217 && y > 0) {
+                        if(x > 340 && x < 600 || x < 260 && x > 0) {
+                            if(mouse.getButton() == 1) {
+                                bet += 10;
+                            }
+                            if(mouse.getButton() == 3) {
+                                bet += -10;
+                            }
+                        }
+                        returnNum = bet;
+                    } else if (y < 233 && y > 217) {
+                        if(x < 340 && x > 260) {
+                            returnNum = -100; //<-This triggers the code above to stop prompting user to place a bet
+                        }
                     }
                 }
             }
