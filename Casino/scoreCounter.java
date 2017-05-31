4a8d708d5b02d50c957c0151e30889900f23e287
B-C-Gallagher <b.c.gallagher24@gmail.com>
2017-05-31 10:34:43 -0700

    Two Words

--- Casino/scoreCounter.java
+++ Casino/scoreCounter.java
@@ -2,7 +2,18 @@ import greenfoot.*;
 import java.awt.Color;  
 public class scoreCounter extends Actor
 {
+    public int Startcash = 200;
+    public int dealerCash = 400; 
+    //scoreCounter scoreKeeper = new scoreCounter();//issue
+    private String userCash() {
+        return "$" + Startcash;
+    }
     public scoreCounter(){
-        setImage(new GreenfootImage( "Score: ", 18, Color.WHITE, Color.BLACK));
+        setImage(new GreenfootImage( "Score: " + userCash(), 25, Color.YELLOW, Color.BLACK));
+    }
+    public void modifyCash(int modNum) {
+        Startcash += modNum;
+        dealerCash -= modNum;
+        setImage(new GreenfootImage( "Score: " + userCash(), 25, Color.YELLOW, Color.BLACK));
     }
 }
